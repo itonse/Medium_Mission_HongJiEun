b625d30afbc91d1a8164a7f3b696bee3282e7452
itonse <govl3dnjs@gmail.com>
2023-12-05 17:18:14 +0900

    feat : Member Entity 설정

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -2,7 +2,20 @@ package com.ll.medium.domain.member.member.entity;
 
 import com.ll.medium.global.jpa.baseEntity.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@SuperBuilder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+@ToString(callSuper = true)
 public class Member extends BaseEntity {
+    private String username;
+    private String password;
+    private String email;
+    private boolean verified;
 }
