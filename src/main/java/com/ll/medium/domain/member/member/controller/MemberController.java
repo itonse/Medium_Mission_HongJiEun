2e4d51f6304cdc5a863f71535617de3fa4379feb
itonse <govl3dnjs@gmail.com>
2023-12-06 00:38:53 +0900

    feat : 회원가입 폼 구성

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,13 +1,28 @@
 package com.ll.medium.domain.member.member.controller;
 
-import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.domain.member.member.dto.JoinForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
-    private final MemberService memberService;
+
+    @PreAuthorize("isAnonymous()")    // 로그인하지 않은 상태여야 메서드에 접근 가능
+    @GetMapping("/join")
+    public String showJoin() {
+        return "domain/member/member/join";
+    }
+
+    @PreAuthorize("isAnonymous()")
+    @PostMapping("/join")
+    public void join(@Valid JoinForm joinForm) {
+        // TODO
+    }
 }
---
+++ src/main/java/com/ll/medium/domain/member/member/dto/JoinForm.java
@@ -0,0 +1,19 @@
+package com.ll.medium.domain.member.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class JoinForm {
+    @NotBlank
+    @Size(min = 4, max = 12)
+    private String username;
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String password;
+    @NotBlank
+    @Email
+    private String email;
+}
