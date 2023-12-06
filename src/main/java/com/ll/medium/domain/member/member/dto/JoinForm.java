e91b446959c12661261822de54dbecdb2108028b
itonse <govl3dnjs@gmail.com>
2023-12-06 23:21:11 +0900

    fix : JoinForm에 @Setter 추가
    회원가입 페이지에서 입력한 값들이 joinForm에 매핑되지 않던 오류 해결

--- src/main/java/com/ll/medium/domain/member/member/dto/JoinForm.java
+++ src/main/java/com/ll/medium/domain/member/member/dto/JoinForm.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class JoinForm {
     @NotBlank
