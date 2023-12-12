a0187fc0599ea6652eb178e1cd31fa6c17011d48
itonse <govl3dnjs@gmail.com>
2023-12-12 14:31:46 +0900

    feat : 게시물 작성 기능 추가
    - 컨트롤러에 showWrite, write 메서드 추가
    - 로그인된 사용자만 글을 쓸 수 있도록 @PreAuthorize("isAuthenticated()") 어노테이션 설정

--- src/main/java/com/ll/medium/domain/post/post/controller/postController.java
+++ src/main/java/com/ll/medium/domain/post/post/controller/postController.java
@@ -1,10 +1,15 @@
 package com.ll.medium.domain.post.post.controller;
 
+import com.ll.medium.domain.post.post.dto.WriteForm;
 import com.ll.medium.domain.post.post.service.PostService;
+import com.ll.medium.global.rq.Rq;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,10 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class postController {
     private final PostService postService;
+    private final Rq rq;
 
     @GetMapping("/list")
     public String list(Model model) {
         model.addAttribute("posts", postService.getPosts());
         return "domain/post/post/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/write")
+    public String showWrite() {
+        return "domain/post/post/write";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/write")
+    public String write(@Valid WriteForm writeForm) {
+        postService.write(writeForm, rq.getUser());
+        return "redirect:/post/list";
     }
 }
--- src/main/java/com/ll/medium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/post/service/PostService.java
@@ -1,14 +1,19 @@
 package com.ll.medium.domain.post.post.service;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.repository.MemberRepository;
+import com.ll.medium.domain.post.post.dto.WriteForm;
 import com.ll.medium.domain.post.post.entity.Post;
 import com.ll.medium.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Slf4j
 @Service
 @Transactional(readOnly = true)
@@ -19,5 +24,21 @@ public class PostService {
 
     public List<Post> getPosts() {
         return postRepository.findAll();
+    }
+
+    @Transactional
+    public void write(WriteForm writeForm, User user) {
+        Optional<Member> authorOptional = memberRepository.findByUsername(user.getUsername());
+
+        authorOptional.ifPresent(author -> {
+            Post post = Post.builder()
+                    .author(author)
+                    .title(writeForm.getTitle())
+                    .body(writeForm.getBody())
+                    .published(writeForm.isPublished())
+                    .build();
+
+            postRepository.save(post);
+        });
     }
 }
