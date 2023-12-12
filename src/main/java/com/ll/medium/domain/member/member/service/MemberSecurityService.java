b973c001c231cf2713bc33226f9171a1e3687c19
itonse <govl3dnjs@gmail.com>
2023-12-12 17:30:42 +0900

    refactor : 주석 추가

--- src/main/java/com/ll/medium/domain/member/member/service/MemberSecurityService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberSecurityService.java
@@ -29,7 +29,7 @@ public class MemberSecurityService implements UserDetailsService {
         }
         Member member = memberOptional.get();
 
-        // 권한 부여
+        // 권한 부여: 아이디가 'admin'일 경우 관리자 권한 부여, 그 외에는 일반 사용자 권한 부여
         List<GrantedAuthority> authorities = new ArrayList<>();
         if ("admin".equals(username)) {
             authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
@@ -37,6 +37,7 @@ public class MemberSecurityService implements UserDetailsService {
             authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
         }
 
-        return new User(member.getUsername(), member.getPassword(), authorities);   // UserDetail 인터페이스 구현체
+        // Spring Security User 객체 반환 (UserDetails 인터페이스의 구현체)
+        return new User(member.getUsername(), member.getPassword(), authorities);
     }
 }
