25863f7a2547f6da9dc4c168f44fa3f931fe4994
itonse <govl3dnjs@gmail.com>
2023-12-13 22:04:47 +0900

    feat : 전역 예외 처리 설정 및 @Valid 예외 처리
    - GlobalExceptionHandler에 MethodArgumentNotValidException 처리 로직을 추가하여 @Valid 어노테이션에 의한 예외를 처리
    - 유효성 검사 실패 시, 오류 메시지를 Rq 클래스의 historyBack 메서드를 통해 사용자에게 전달.
    - js.html 페이지에서 알림창을 통해 전달받은 오류 메시지를 사용자에게 표시하도록 수정

---
+++ src/main/java/com/ll/medium/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.ll.medium.global.exception;
+
+import com.ll.medium.global.rq.Rq;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+@ControllerAdvice
+@Slf4j
+@RequiredArgsConstructor
+@Order(-1)    // 다른 예외 처리 클래스보다 우선 순위로 적용되는 클래스
+public class GlobalExceptionHandler {
+    private final Rq rq;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidException(MethodArgumentNotValidException exception) {
+        List<String> errors = new ArrayList<>();
+        exception.getBindingResult().getAllErrors()
+                .forEach((error -> errors.add(error.getDefaultMessage())));
+
+        String joinedErrors = String.join(", ", errors);
+        log.info(errors.toString());
+        return rq.historyBack(joinedErrors);
+    }
+}
