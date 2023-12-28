51bf91df955ce6df799b837911a206ca0e8d8422
itonse <govl3dnjs@gmail.com>
2023-12-29 01:29:54 +0900

    feat : admin에게 PAID 권한 적용

--- src/main/java/com/ll/medium/domain/member/member/service/MemberSecurityService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberSecurityService.java
@@ -34,6 +34,7 @@ public class MemberSecurityService implements UserDetailsService {
         List<GrantedAuthority> authorities = new ArrayList<>();
         if ("admin".equals(username)) {
             authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+            authorities.add(new SimpleGrantedAuthority(UserRole.PAID.getValue()));
         } else if (member.isPaid()) {
             authorities.add(new SimpleGrantedAuthority(UserRole.PAID.getValue()));
         } else {
